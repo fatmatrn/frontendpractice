f77b2bf5f81d0431113d3babb3ad749bc4b71ae6
Fatmatrn <ftmtrn3488@gmail.com>
2023-05-19 01:03:14 +0300

    5

--- Replit/dom/script.js
+++ Replit/dom/script.js
@@ -1,14 +1,17 @@
 const txtName = document.getElementById("txtName");
 const txtPoint = document.getElementById("txtPoint");
 const btnAdd = document.getElementById("btnAdd");
-
+let id = 1;
 
 btnAdd.addEventListener("click",()=>{
-     const html = `  <tr>
-     <td>#</td>
+     let html ="";
+      html+=`  <tr>
+     <td>${id++}</td>
      <td>${txtName.value}</td>
      <td>${txtPoint.value}</td>
      <td class="d-flex justify-content-around " ><i class="fa-solid  fa-pencil "></i> 
          <i class="fa-solid   fa-trash-can"></i></td>
  </tr>`;
+document.querySelector("tbody").innerHTML+=html;
+ 
 });
