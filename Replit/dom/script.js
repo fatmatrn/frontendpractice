81b7b24333c5eb7cbf9f13a6c106d448df133fed
Fatmatrn <ftmtrn3488@gmail.com>
2023-05-21 11:42:01 +0300

    4

--- Replit/dom/script.js
+++ Replit/dom/script.js
@@ -2,7 +2,11 @@ const txtName = document.getElementById("txtName");
 const txtPoint = document.getElementById("txtPoint");
 const btnAdd = document.getElementById("btnAdd");
 const tbodyEl = document.querySelector("tbody");
-
+const v1 = `<i class="fa-solid fa-pencil margin"></i>
+<i class="fa-solid fa-trash-can"></i
+>`;
+const v2 = `<i class="fa-solid fa-check margin"></i>
+<i class="fa-solid fa-xmark"></i>`;
 
 btnAdd.addEventListener("click",()=>{
     
@@ -12,14 +16,12 @@ btnAdd.addEventListener("click",()=>{
     let tdId = document.createElement("td"); 
     let tdName = document.createElement("td"); 
     let tdPoint = document.createElement("td"); 
-    let tdLast = document.createElement("td").classList.add("d-flex gap-2"); 
+    let tdLast = document.createElement("td"); 
    
     tdName.innerHTML=txtName.value;
     console.log(txtName.value)
-    tdPoint.innerHTML=tdPoint.value;
-    tdLast.innerHTML = `<i class="fa-solid fa-pencil">
-    <i class="fa-solid fa-trash-can"></i
-  ></i>`;
+    tdPoint.innerHTML=txtPoint.value;
+    tdLast.innerHTML = v1;
 
   trEl.appendChild(tdId);
   trEl.appendChild(tdName);
@@ -28,11 +30,44 @@ btnAdd.addEventListener("click",()=>{
 
   tbodyEl.prepend(trEl);
 
-  let tdInTbody = document.querySelectorAll("tbody tr td:first-child")
+  tdLast.classList.add("text-center");
+
+  let tdInTbody = tbodyEl.querySelectorAll("tr td:first-child")
 
 
   for(let i in tdInTbody){
     tdInTbody[i].innerText=i+1;
   }
+  txtName.value="";
+  txtPoint.value="";
+  txtName.focus();
  
 });
+
+tbodyEl.addEventListener("click",(e)=>{
+   let cells =  e.target.parentElement.parentElement.querySelectorAll("td");
+
+if(e.target.classList.contains("fa-pencil")){
+
+cells[3].innerHTML=v2;
+
+ cells[1].setAttribute("contentEditable", true);
+ cells[2].setAttribute("contentEditable", true);
+ cells[1].focus();
+
+}else if(e.target.classList.contains("fa-trash-can")){
+  e.target.parentElement.parentElement.remove();
+
+  let tdInTbody = tbodyEl.querySelectorAll("tr td:first-child")
+  
+  for(let i in tdInTbody){
+    console.log(i)
+    tdInTbody[i].innerText=`${i*1}+1`;
+  }
+
+}else if(e.target.classList.contains("fa-check")){
+
+  cells[3].innerHTML=v1;
+
+}
+})
