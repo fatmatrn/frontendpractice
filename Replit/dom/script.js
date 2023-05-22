db6e4a14197af2963160ff1821fa02d9d26b8e26
Fatmatrn <ftmtrn3488@gmail.com>
2023-05-22 16:42:58 +0300

    4

--- Replit/dom/script.js
+++ Replit/dom/script.js
@@ -2,16 +2,19 @@ const txtName = document.getElementById("txtName");
 const txtPoint = document.getElementById("txtPoint");
 const btnAdd = document.getElementById("btnAdd");
 const tbodyEl = document.querySelector("tbody");
+const avg = document.querySelector(".avg");
 const v1 = `<i class="fa-solid fa-pencil margin"></i>
 <i class="fa-solid fa-trash-can"></i
 >`;
 const v2 = `<i class="fa-solid fa-check margin"></i>
 <i class="fa-solid fa-xmark"></i>`;
 
+let previousName = "";
+let previousPoint = "";
+
 btnAdd.addEventListener("click",()=>{
     
     let trEl = document.createElement("tr");
-    console.log(trEl)
     
     let tdId = document.createElement("td"); 
     let tdName = document.createElement("td"); 
@@ -19,7 +22,6 @@ btnAdd.addEventListener("click",()=>{
     let tdLast = document.createElement("td"); 
    
     tdName.innerHTML=txtName.value;
-    console.log(txtName.value)
     tdPoint.innerHTML=txtPoint.value;
     tdLast.innerHTML = v1;
 
@@ -35,9 +37,20 @@ btnAdd.addEventListener("click",()=>{
   let tdInTbody = tbodyEl.querySelectorAll("tr td:first-child")
 
 
-  for(let i in tdInTbody){
-    tdInTbody[i].innerText=i+1;
-  }
+  for (let i = 0; i < tdInTbody.length; i++){
+        
+    tdInTbody[i].innerText = i + 1;       
+}
+
+
+let tdInTbodyAvg = tbodyEl.querySelectorAll("tr td:nth-child(3)")
+
+let sum =0;
+for (let i = 0; i < tdInTbodyAvg.length; i++){
+    sum+=Number(tdInTbodyAvg[i].innerText);
+}
+avg.innerHTML=(sum/tdInTbodyAvg.length).toFixed(2);
+
   txtName.value="";
   txtPoint.value="";
   txtName.focus();
@@ -45,29 +58,72 @@ btnAdd.addEventListener("click",()=>{
 });
 
 tbodyEl.addEventListener("click",(e)=>{
+
    let cells =  e.target.parentElement.parentElement.querySelectorAll("td");
 
 if(e.target.classList.contains("fa-pencil")){
 
-cells[3].innerHTML=v2;
+ for(let x of tbodyEl.querySelectorAll("td:last-child")){
+  x.parentElement.querySelectorAll("td")[1].setAttribute("contentEditable", false);
+  x.parentElement.querySelectorAll("td")[2].setAttribute("contentEditable", false);
+  x.innerHTML=v1;
+ }
+
+ previousName=cells[1].innerText;
+ previousPoint=cells[2].innerText;
+
+  cells[3].innerHTML=v2;
 
  cells[1].setAttribute("contentEditable", true);
  cells[2].setAttribute("contentEditable", true);
  cells[1].focus();
 
+
+
+
+
+
 }else if(e.target.classList.contains("fa-trash-can")){
+//   let deleteItem = true;
+// if (confirm("Are you sure you want to delete this?")) {
+//   deleteItem = true;
+// } else {
+//   deleteItem = false;
+// }
+
+let deleteItem = (confirm("Are you sure you want to delete this?"))? true : false;
+
+if (deleteItem) {
   e.target.parentElement.parentElement.remove();
+}
+ 
 
   let tdInTbody = tbodyEl.querySelectorAll("tr td:first-child")
+
   
-  for(let i in tdInTbody){
-    console.log(i)
-    tdInTbody[i].innerText=`${i*1}+1`;
-  }
+ for (let i = 0; i < tdInTbody.length; i++){
+        
+        tdInTbody[i].innerText = i + 1;       
+    }
 
-}else if(e.target.classList.contains("fa-check")){
+    let tdInTbodyAvg = tbodyEl.querySelectorAll("tr td:nth-child(3)")
 
-  cells[3].innerHTML=v1;
-
+let sum =0;
+for (let i = 0; i < tdInTbodyAvg.length; i++){
+    sum+=Number(tdInTbodyAvg[i].innerText);
 }
+avg.innerHTML=(sum/tdInTbodyAvg.length).toFixed(2);
+
+} else if(e.target.classList.contains("fa-check")){
+
+    cells[1].setAttribute("contentEditable", false);
+    cells[2].setAttribute("contentEditable", false);
+    cells[3].innerHTML=v1;
+
+   }else if(e.target.classList.contains("fa-xmark")){
+
+    cells[1].innerText=previousName;
+    cells[2].innerText=previousPoint;
+    cells[3].innerHTML=v1;
+   }
 })
